46eb3b081f2eaabe2d5f2c12c13b5a55f6afb0c4
Pavel-P <pavapil@gmail.com>
2023-09-18 10:39:19 -0400

    Remove world file argument in launcher

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -15,10 +15,6 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     # Set the path to this package.
     package_path = get_package_share_directory("peg_puzzle_gazebo")
-    
-    # Set the path to the world file
-    world_file_name = 'peg_puzzle.world'
-    world_path = os.path.join(package_path, 'worlds', world_file_name)
 
     gazebo_models_path = os.path.join(package_path, 'models')
     os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
@@ -29,7 +25,6 @@ def generate_launch_description():
             [FindPackageShare("gazebo_ros"), "/launch", "/gazebo.launch.py"]
         ),
         launch_arguments={
-            'world': world_path,
             }.items()
     )
 
